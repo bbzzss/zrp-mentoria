5c517bb452db2097fc2a0e02e646da6bf7a50905
João <jlcarruda3@gmail.com>
2023-08-16 15:10:15 -0300

    feat: login endpoint

--- index.js
+++ index.js
@@ -16,19 +16,44 @@ connect('mongodb://mongo:27017/mentoria').then(() => {
   app.get('/', function (req, res) {
     res.send('Hello World!')
   })
+
   app.post('/register', function (req, res) {
     User.create({
       name: req.body.name,
       email: req.body.email,
       password: req.body.password
     }).then((user) => {
-      console.log(user.toJSON())
       res.status(201).json(user.toJSON())
     }).catch((error) => {
       console.error(error.message)
       res.status(500).send()
     })
   })
+
+  app.post('/login', function (req, res) {
+    const { email, password } = req.body
+
+    User.findOne({
+      email // : email
+    }).then((user) => {
+      if (password === user.password) {
+        const token = "abcd1234-" + user._id
+        res.status(200).json({ token })
+      }
+
+      return Promise.reject()
+    }).catch(() => {
+      res.status(501).send()
+    })
+
+    // LOGIN TODO
+    // - Recebe email
+    // - Consultar pra ver se usuario existe
+    // - Encriptar a senha que é passada pelo cliente, e comparar com a do banco
+    // - Valida se esta no banco
+    // - 
+  })
+
   app.listen(port, function () {
     console.log('Aplicação escutando na porta 3000')
   })
